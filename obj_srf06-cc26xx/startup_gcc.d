94e0ddd098857b6ee0663d9e001270cd05886d1e
weijian19391 <weijian19391@gmail.com>
2016-02-04 19:09:45 +0800

    clean files

--- obj_srf06-cc26xx/startup_gcc.d
+++
@@ -1,7 +0,0 @@
-obj_srf06-cc26xx/startup_gcc.o: \
- contiki/cpu/cc26xx-cc13xx/lib/cc26xxware/startup_files/startup_gcc.c \
- contiki/cpu/cc26xx-cc13xx/lib/cc26xxware/inc/hw_types.h \
- contiki/cpu/cc26xx-cc13xx/lib/cc26xxware/inc/hw_chip_def.h
- contiki/cpu/cc26xx-cc13xx/lib/cc26xxware/startup_files/startup_gcc.c :
- contiki/cpu/cc26xx-cc13xx/lib/cc26xxware/inc/hw_types.h :
- contiki/cpu/cc26xx-cc13xx/lib/cc26xxware/inc/hw_chip_def.h :
